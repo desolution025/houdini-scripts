fe967f9970dd5a1a8d12d479fed088fa00adefbe
desolution025 <1484961843@qq.com>
2022-08-30 22:35:46 +0800

    complete_auto_mat

---
+++ auto_gen_pbr.py
@@ -0,0 +1,131 @@
+from collections import defaultdict
+from os import PathLike
+from typing import List, Tuple, Sequence, Mapping, Optional
+import re
+try:
+    from pathlib2 import Path
+except ImportError:
+    from pathlib import Path
+try:
+    import ujson as json
+except ImportError:
+    import json
+from loguru import logger
+
+
+DIFFUSE = ("diffuse", "albedo", "color", "basecolor", "diff")
+SPECULAR = ("specular", "spec")
+ROUGHNESS = ("roughness",)
+GLOSSINESS = ("glossiness",)
+NORMAL = ("normal", "nrm")
+BUMP = ("bump", "bmp")
+HEIGHT = ("height",)
+DISPLACEMENT = ("displacement", "disp")
+OPACITY = ("opacity", "opac")
+TRANSLUCENCY = ("translucency",)
+TRANSMISSION = ("transmission",)
+
+TEXSUFFIX = ('exr','png', 'jpg', 'jpeg')
+
+
+
+class PBR_Tex_Folder:
+
+    channels_mapping = {
+                    "Diffuse": ["diffuse", "albedo", "basecolor", "color", "diff"],
+                    "Specular": ["specular", "spec"],
+                    "Metalness": ["metalness"],
+                    "Roughness": ["roughness", "rough"],
+                    "Glossiness": ["glossiness", "gloss"],
+                    "Normal": ["normal", "nrm"],
+                    "Bump": ["bump", "bmp"],
+                    "Height": ["height",],
+                    "Displacement": ["displacement", "disp"],
+                    "Opacity": ["opacity", "opac"],
+                    "Translucency": ["translucency"],
+                    "Transmission": ["transmission"],
+                    "Emission": ["emission"]
+                }
+    tex_suffix = ['exr','png', 'jpg', 'jpeg']
+
+    def __init__(self, path: PathLike,
+                channels: Sequence[str]=[
+                    "Diffuse",
+                    "Specular", 
+                    "Metalness",
+                    "Roughness",
+                    "glossiness",
+                    "Normal",
+                    "Displacement",
+                    "Opacity",
+                    "Translucency",
+                    "Transmission",
+                    "Emission"
+                ],
+                texsuffix: Sequence[str]=['exr','png', 'jpg', 'jpeg'],
+                *,
+                mtlprefix: Optional[str]=None,
+                subfolder: Optional[PathLike]=None) -> None:
+        self.path, self.channels, self.texsuffix, self.mtlprefix, self.subfolder = Path(path), channels, texsuffix, mtlprefix, subfolder
+        self.not_matched = None  # A list for files not matched
+
+    def all_tex(self) -> List[PathLike]:
+        texs = []
+        for suf in TEXSUFFIX:
+            texs.extend(self.path.glob(f'*.{suf}'))
+        logger.debug(f'ALL texures in folder: {list(map(lambda x: x.name, texs))}')
+        return texs
+
+    def parse_channel(self, channel: str) -> Optional[Tuple[Mapping[str, PathLike], Mapping[str, List[PathLike]]]]:
+        texs = {}
+        repeated_files = defaultdict(list)
+        if self.not_matched is None:
+            self.not_matched = self.all_tex()
+
+        type_feature = "|".join(self.__class__.channels_mapping[channel])
+        suffix_feature = "|".join(self.texsuffix)
+        repattern = rf"(.*?)({type_feature}).*?\.({suffix_feature})"
+        logger.debug(f"Filter files with rule: {repattern}")
+
+        for f in self.not_matched:
+            match = re.match(repattern, f.name, flags=re.I)
+
+            if match:
+                prefix_ = match.group(1).rstrip('_')
+                if prefix_ in texs:
+                    logger.info(f"There is already matched a {channel} texture for material {prefix_}, ignore file: {f.name}")
+                    repeated_files[f"{match.group(1)}{channel}"].append(str(f))
+                    continue
+                texs[prefix_] = f
+                logger.debug(f"Matched file: {f}")
+
+        if texs:
+            for f in texs.values():
+                self.not_matched.remove(f)  # Remove files that has matched
+            return texs, repeated_files
+
+        if not texs:  # No file match
+            logger.info(f"Did not found file matching rule of channel {channel}")
+
+    def makeup_mtl(self):
+        mtls = defaultdict(dict)
+        varians = defaultdict(dict)
+        for c in self.channels:
+            result = self.parse_channel(c)
+            if result is not None:
+                texs, repeated_files = result
+                for m, t in texs.items():
+                    mtls[m][c] = t
+                for m ,l in repeated_files.items():
+                    varians[m][c] = l
+        return mtls, varians
+
+
+
+if __name__ == "__main__":
+    folder1 = r"F:\3D Models\Model Pack\KitBash3D\KitBash3D - Mini Kit Aristocracy\KB3DTextures"
+    folder2 = r"F:\Footage\Magascan Libary\Downloaded\3d\3d_assembly_uhqibgfga"
+
+    pmf = PBR_Tex_Folder(folder1)
+    dd, va = pmf.makeup_mtl()
+    print(dd)
--- gen_pbr_mat.py
+++ gen_pbr_mat.py
@@ -1,13 +1,13 @@
-from pathlib import Path
-
+from typing import Mapping
+from os import PathLike
+from loguru import logger
 import hou
-
 
 def isMatnet(node: hou.Node):
     return node.path() == "/mat" or isinstance(node, hou.ShopNode) or node.type().name() == "materiallibrary"
 
 
-def getCurMatnet():
+def getCurMatnet() -> hou.Node:
     """Find active matnet"""
     panes :tuple[hou.PaneTab] = [p for p in hou.ui.currentPaneTabs() if isinstance(p, hou.NetworkEditor) and isMatnet(p.pwd())]
 
@@ -24,3 +24,111 @@ def getCurMatnet():
     # return the matnet which was first found
     return cur[0].pwd()
     
+COLOR_CHANNELS = ["Diffuse", "Specular", "Translucency", "Transmission", "Emisson"]
+FLOAT_CHANNELS = ["Metalness", "Roughness", "Glossiness", "Bump", "Height"]
+
+CONNECT_MAPPING = {
+    "MtlX": {
+        "Diffuse": 1,
+        "Specular": 5,
+        "Metalness": 3,
+        "Roughness": 6,
+        "Normal": 40,
+        "Opacity": 38,
+        "Translucency": 11,
+        "Transmission": 10,
+        "Emission": 37
+    }
+}
+
+
+def gen_mat(parent: hou.Node, name: str, texs: Mapping[str, PathLike]):
+    # Generate material subnetwork
+    matnet : hou.Node = parent.createNode("subnet", name)
+    
+    # Set Karma MeterialX Mask
+    mask_parm = hou.StringParmTemplate("tabmenumask", "TabMenuMask", 1)
+    parmfolder = hou.FolderParmTemplate("folder1", "KARMA MASK", ((mask_parm,)), hou.folderType.Collapsible)
+    parmgroup = hou.ParmTemplateGroup((parmfolder,))
+
+    matnet.setParmTemplateGroup(parmgroup)
+    matnet.parm("tabmenumask").set("MaterialX parameter collect subnet null subnetconnector karma USD")
+
+    # Define ouput surface
+    surfaceoutput : hou.Node = matnet.createNode("subnetconnector", "surface_output")
+    surfaceoutput.parm("parmname").set("surface")
+    surfaceoutput.parm("parmlabel").set("Surface")
+    surfaceoutput.parm("parmtype").set("surface")
+    surfaceoutput.parm("connectorkind").set("output")
+
+    # Define output displacement
+    dispoutput : hou.Node = matnet.createNode("subnetconnector", "displacement_output")
+    dispoutput.parm("parmname").set("displacement")
+    dispoutput.parm("parmlabel").set("Displacement")
+    dispoutput.parm("parmtype").set("displacement")
+    dispoutput.parm("connectorkind").set("output")
+
+    # Create MaterialX Standard
+    mtlx : hou.Node =  matnet.createNode("mtlxstandard_surface", "surface_mtlx")
+    surfaceoutput.setInput(0, mtlx)
+
+    # Set surface channels for material
+    mtlx_mapping = CONNECT_MAPPING["MtlX"]
+
+    for channel in mtlx_mapping:
+        # Normal 
+        if channel == "Normal" and "Normal" in texs:
+            normal = matnet.createNode("mtlximage", "Normal")
+            normal.parm("signature").set("vector3")
+            plugnormal = matnet.createNode("mtlxnormalmap" )
+            normal.parm("file").set(str(texs["Normal"]))
+            mtlx.setInput(mtlx_mapping["Normal"], plugnormal)
+            plugnormal.setInput(0, normal)
+
+        elif channel == "Roughness" and "Roughness" not in texs and "Glossiness" in texs:
+            gloss = matnet.createNode("mtlximage", "Glossiness")
+            gloss.parm("signature").set("0")
+            substract = matnet.createNode("mtlxsubtract" )
+            gloss.parm("file").set(str(texs["Glossiness"]))
+            mtlx.setInput(mtlx_mapping["Roughness"], substract)
+            substract.parm("in1").set(1)
+            substract.setInput(1, gloss)
+
+        elif channel in texs:
+            img : hou.Node = matnet.createNode("mtlximage", channel)
+            img.parm("file").set(str(texs[channel]))
+            mtlx.setInput(mtlx_mapping[channel], img)
+            if channel in FLOAT_CHANNELS:
+                img.parm("signature").set("0")
+            elif channel not in COLOR_CHANNELS:
+                img.parm("signature").set("vector")
+        
+    # Set displacement
+    if "Displacement" in texs:
+        #CREATING DISPLACE NODES
+        displace = matnet.createNode("mtlximage", "Displacement")
+        plugdisplace = matnet.createNode("mtlxdisplacement" )
+        remapdisplace = matnet.createNode("mtlxremap", "Offset_Displacement" )
+        #SETTING PARAMETERS DISPLACE
+        #set remap
+        remapdisplace.parm("outlow").set(-0.5)
+        remapdisplace.parm("outhigh").set(0.5)
+        #set scale displace
+        plugdisplace.parm("scale").set(0.1)
+        #set image displace
+        displace.parm("file").set(str(texs["Displacement"]))
+        displace.parm("signature").set("0")
+        
+        #SETTING INPUTS
+        dispoutput.setInput(0, plugdisplace)
+        plugdisplace.setInput(0, remapdisplace)
+        remapdisplace.setInput(0, displace)
+    
+    matnet.layoutChildren()
+    logger.info(f"Create Material {name} in {matnet.path()}")
+    return matnet
+
+def gen_mats(textures: Mapping[str, Mapping[str, PathLike]]):
+    parent = getCurMatnet()
+    mats = [gen_mat(parent, name, texs) for name, texs in textures.items()]
+    parent.layoutChildren(items=mats)
